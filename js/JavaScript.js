7fa25e3c8cda3b41de8a89d716a9485ed660f314
Stefon Johns <stefonjohns2@gmail.com>
2017-09-16 15:35:39 -0700

    Second Attempt at Site Update
    trying to update site once again

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,2 +1,23 @@
-$('.navbar').localScroll();
-$('.container').localScroll();
+$('.carousel').carousel({
+  interval: 3000
+});
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+AOS.init();
+
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+});
