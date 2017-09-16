b9a5e45676236cb13565124bad75f14cdfdbe802
Stefon Johns <stefonjohns2@gmail.com>
2017-09-16 15:02:54 -0700

    Updates to Website
    --Did a complete redo of my personal webste.
    --added scroll animations as well as color scheme!

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,2 +1,26 @@
-$('.navbar').localScroll();
-$('.container').localScroll();
+$('.carousel').carousel({
+  interval: 3000
+});
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+AOS.init();
+
+AOS.init({
+  disable: 'mobile'
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+        })
