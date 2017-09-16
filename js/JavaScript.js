472b80cb3a1a796d442a94d8963382851a55357f
Stefon Johns <stefonjohns2@gmail.com>
2017-09-16 15:38:33 -0700

    Update Failed
    Update failed yet again. Reverting to prior site.

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,23 +1,2 @@
-$('.carousel').carousel({
-  interval: 3000
-});
-
-$('a[href^="#"]').on('click', function(event) {
-
-    var target = $(this.getAttribute('href'));
-
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-
-});
-
-AOS.init();
-
-
-$(function () {
-    $('[data-toggle="popover"]').popover()
-});
+$('.navbar').localScroll();
+$('.container').localScroll();
